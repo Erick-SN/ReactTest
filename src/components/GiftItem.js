3663eef821b6bd2f4cadbfb65c256280145bdda0
Erick-SN <ericksn0707@gmail.com>
2021-02-18 12:44:14 -0600

    animateCss

--- src/components/GiftItem.js
+++ src/components/GiftItem.js
@@ -1,7 +1,7 @@
 const GiftItem = ({ id, title, url }) => {
   return (
     <>
-      <div className="card">
+      <div className="card animate__animated animate__fadeIn">
         <img src={url} alt="animated" />
         <p>{title}</p>
       </div>
