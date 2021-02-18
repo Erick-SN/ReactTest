9695fd4b2096b348ffa18672705e0d91806a7605
Erick-SN <ericksn0707@gmail.com>
2021-02-18 15:28:08 -0600

    Test GiftItem end'

--- src/components/GiftItem.js
+++ src/components/GiftItem.js
@@ -3,7 +3,7 @@ const GiftItem = ({ title, url }) => {
   return (
     <>
       <div className="card animate__animated animate__fadeIn">
-        <img src={url} alt="animated" />
+        <img src={url} alt={title} />
         <p>{title}</p>
       </div>
     </>
--- src/tests/components/GiftItem.test.js
+++ src/tests/components/GiftItem.test.js
@@ -3,10 +3,19 @@ import GiftItem from '../../components/GiftItem';
 
 describe('Pruebas en <GiftItem/>', () => {
   let wrapper;
+  const title = 'title';
+  const url = 'http://';
   beforeEach(() => {
-    wrapper = shallow(<GiftItem title="Image" url="http://" />);
+    wrapper = shallow(<GiftItem title={title} url={url} />);
   });
   test('Renderizado de componente', () => {
-    expect(wrapper).toMatchInlineSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+  test('Asegurando los elemtos para mostrar la imagen', () => {
+    const p = wrapper.find('p');
+    const img = wrapper.find('img');
+    expect(p.text().trim()).toBe(title);
+    expect(img.prop('src')).toBe(url);
+    expect(img.prop('alt')).toBe(title);
   });
 });
